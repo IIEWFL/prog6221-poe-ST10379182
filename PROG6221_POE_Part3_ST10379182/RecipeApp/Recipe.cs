640507b0290113fb5e5ff0b810edaaa5bdec12d1
Liam <131532595+ST10379182@users.noreply.github.com>
2024-06-24 18:55:06 +0200

    3rd Commit

--- PROG6221_POE_Part3_ST10379182/RecipeApp/Recipe.cs
+++ PROG6221_POE_Part3_ST10379182/RecipeApp/Recipe.cs
@@ -7,6 +7,9 @@ namespace RecipeApp
     public class Recipe
     {
         public string Name { get; set; }
+        public List<string> Ingredients { get; set; }
+        public string Steps { get; set; }
 
+        public Recipe(string name, List<string> ingredients, string steps)
 
 // https://learn.microsoft.com/en-us/dotnet/csharp/linq/ //
