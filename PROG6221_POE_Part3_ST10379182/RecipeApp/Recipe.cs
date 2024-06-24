e5d4af5ee7200012116fa987459b8fa852a45a3b
Liam <131532595+ST10379182@users.noreply.github.com>
2024-06-24 18:32:10 +0200

    2nd Commit

--- PROG6221_POE_Part3_ST10379182/RecipeApp/Recipe.cs
+++ PROG6221_POE_Part3_ST10379182/RecipeApp/Recipe.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 
 namespace RecipeApp
 {
- 
+
+    public class Recipe
+    {
+        public string Name { get; set; }
 
 
 // https://learn.microsoft.com/en-us/dotnet/csharp/linq/ //
